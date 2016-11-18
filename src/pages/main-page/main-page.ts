ba9f1b15354dcbef9c7050d4d771c576f45a1f76
oliviercherrier <olivier.cherrier@gmail.com>
2016-11-18 19:00:02 +0100

    Real time position display

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -19,10 +19,9 @@ export class MainPage {
   private map: any;
   private nav: NavController;
   private events: Events;
-
+  public locations; // Polyline for leaflet
   options: any;
   is_not_android: any;
-  locations: any;
   path: any;
   isTracking: any;
   postingEnabled: any;
@@ -50,7 +49,6 @@ export class MainPage {
 
 
     this.path = undefined;
-    this.locations = [];
     this.isTracking = false;
     this.postingEnabled = false;
     this.online = false;
@@ -74,20 +72,8 @@ export class MainPage {
   ionViewDidEnter (){
     this.map = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(this.map);
-    var latlngs = [
-        [48.787105, 2.456750],
-        [48.780757, 2.459475],
-        [48.767011, 2.472028]
-    ];
-    var polyline = L.polyline(latlngs, {color: 'red'}).addTo(this.map);
-    // zoom the map to the polyline
-    this.map.fitBounds(polyline.getBounds());
 
-    /*let runLayer = omnivore.gpx("assets/gpx/Mollard.gpx");
-    runLayer.on('ready', () => {
-        this.map.fitBounds(runLayer.getBounds());
-      });
-    runLayer.addTo(this.map);*/
+    this.locations = L.polyline([], {color: 'red'}).addTo(this.map);
 
   }
 
@@ -144,6 +130,7 @@ export class MainPage {
     BackgroundGeolocation.isLocationEnabled().then(
       this.onLocationCheck
     );
+
   }
 
   stopTracking() {
@@ -200,28 +187,16 @@ export class MainPage {
   onClickReset() {
     console.log("!!!DEMO : onClickReset");
     // Clear prev location markers.
-    let locations = this.locations;
-    for (let n=0, len=locations.length;n<len;n++) {
-      locations[n].setMap(null);
-    }
-    this.locations = [];
   }
 
   // Update screen according to new location
   setCurrentLocation(location) {
     console.log("!!!DEMO : setCurrentLocation, location: { " + Number(location.latitude) + ", ", Number(location.longitude) + "}");
     console.log(location);
-
-    // Draw path into map
-    BackgroundGeolocation.getLocations().then(
-      (res) => {
-        this.updateLeafletMap(res);
-      }
-    );
-  }
-
-  updateLeafletMap(locations){
-
+    
+    // Update leaflet map
+    this.locations.addLatLng([location.latitude, location.longitude]);
+    this.map.fitBounds(this.locations.getBounds());
   }
 
   // Only used for non android device
