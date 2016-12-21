29262621067a9dad98315a380bab97d028fd47cf
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-21 19:49:12 +0100

    Connected to ovh server to tag the version

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-    static REST_API_ADDRESS = "http://192.168.43.103:3000/mauron85-bgeoloc-api";
-    // static REST_API_ADDRESS = "http://vps342125.ovh.net:3000/mauron85-bgeoloc-api";
+    //static REST_API_ADDRESS = "http://192.168.0.14:3000/mauron85-bgeoloc-api";
+    static REST_API_ADDRESS = "http://vps342125.ovh.net:3000/mauron85-bgeoloc-api";
 }
